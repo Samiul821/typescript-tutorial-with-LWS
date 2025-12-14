b8f154cd8b3272376b249aa1791b7abc3dbcae31
Samiul821 <smsamiul821@gmail.com>
2025-12-14 16:30:55 +0600

    type of check basics

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,32 @@
-const country = "I love Bangladesh";
-console.log(country);
+// TYPE
 export {};
+// let playerName = "Tamim Iqbal";
+// let age = 35
+// console.log(playerName);
+// age = "Sakib"
+// let playerName;
+// console.log(playerName);
+// playerName = "Sakib";
+// playerName = 36;
+// console.log(playerName);
+// FUNTION
+// function multiply(a : number, b : number) {
+//     return a * b
+// }
+// console.log(multiply("samiul", 4));
+// ARRAY
+// let fruts = []
+// fruts.push(34)
+// fruts.push("Samiul")
+// console.log(fruts);
+// let mixed = ['apple', 5, true, {}]
+// mixed.push({
+//     name: 'samiul'
+// })
+// OBJECT
+// let person = {
+//     name: "Mashrafi",
+//     age: 35,
+//     isCaptain: true
+// }
+// person.country = "Bangladesh"
